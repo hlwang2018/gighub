fad139f4f933b037c780e27db7a75cc061a44bc1
LAPTOP-FNOI4GUD\Hengliang <hengliangwang@yahoo.com>
2018-04-06 14:18:40 -0400

    Add attendee and artist on the calendar.

---
+++ GigHub/Controllers/Attendancesontroller.cs
@@ -0,0 +1,47 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    public class AttendanceDto
+    {
+        public int GigId { get; set; }
+    }
+
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+            if(_context.Attendances.Any(a => a.AttendeeId == User.Identity.GetUserId() && a.GigId == dto.GigId))
+            {
+                return BadRequest("The attendance already exists.");
+            }
+            
+            var attendance = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
